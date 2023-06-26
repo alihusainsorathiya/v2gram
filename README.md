19d5e978041ece6a66f7b6dfe9c6a10a9306b072
alihusainsorathiya <husain12121@gmail.com>
2023-06-26 16:03:46 +0400

    Adding V2Gram v 1.0

---
+++ README.md
@@ -0,0 +1,33 @@
+# AliV2rayCollector
+
+[![Collect Configs](https://github.com/alihusainsorathiya/v2gram/actions/workflows/php.yml/badge.svg)](https://github.com/alihusainsorathiya/v2gram/actions/workflows/php.yml)
+
+This is a PHP script that collects V2Ray subscription links from various Telegram channels and saves them to different files based on their protocol type (VMess, VLess, Trojan, and Shadowsocks). The collected links are stored in text format.
+
+## How it Works
+The script includes two modules `getv2ray.php` and `config.php`. 
+
+The `config.php` file contains an array of Telegram channels and the types of protocols they offer for V2ray subscriptions. 
+
+The `getv2ray.php` module is responsible for fetching the latest subscription links from a Telegram channel and formatting them as plain text. It takes three arguments: `$channel` represents the Telegram channel name, `$type` represents the type of V2Ray protocol, and `$format` specifies the format of the output (text or JSON).
+
+The main script iterates over each channel and the protocol types they offer, then passes the appropriate arguments to `getv2ray.php` to retrieve the subscription links. The collected links are then concatenated into a single string and saved to individual files based on their protocol type.
+
+## Usage
+Just import the following subscription link into the corresponding client. Use a client that at least support ss + vless + vmess + trojan.
+
+| CONFIG TYPE | NORMAL SUBSCRIPTION | BASE64 SUBSCRIPTION |
+|---|---|---|
+| MIX of ALL | [NORMAL SUBSCRIPTION](https://raw.githubusercontent.com/alihusainsorathiya/v2gram/main/sub/mix) | [BASE64 SUBSCRIPTION](https://raw.githubusercontent.com/alihusainsorathiya/v2gram/main/sub/mix_base64) |
+| VMESS | [NORMAL SUBSCRIPTION](https://raw.githubusercontent.com/alihusainsorathiya/v2gram/main/sub/vmess) | [BASE64 SUBSCRIPTION](https://raw.githubusercontent.com/alihusainsorathiya/v2gram/main/sub/vmess_base64) |
+| VLESS | [NORMAL SUBSCRIPTION](https://raw.githubusercontent.com/alihusainsorathiya/v2gram/main/sub/vless) | [BASE64 SUBSCRIPTION](https://raw.githubusercontent.com/alihusainsorathiya/v2gram/main/sub/vless_base64) |
+| REALITY | [NORMAL SUBSCRIPTION](https://raw.githubusercontent.com/alihusainsorathiya/v2gram/main/sub/reality) | [BASE64 SUBSCRIPTION](https://raw.githubusercontent.com/alihusainsorathiya/v2gram/main/sub/reality_base64) |
+| TROJAN | [NORMAL SUBSCRIPTION](https://raw.githubusercontent.com/alihusainsorathiya/v2gram/main/sub/trojan) | [BASE64 SUBSCRIPTION](https://raw.githubusercontent.com/alihusainsorathiya/v2gram/main/sub/trojan_base64) |
+| ShadowSocks | [NORMAL SUBSCRIPTION](https://raw.githubusercontent.com/alihusainsorathiya/v2gram/main/sub/shadowsocks) | [BASE64 SUBSCRIPTION](https://raw.githubusercontent.com/alihusainsorathiya/v2gram/main/sub/shadowsocks_base64) |
+
+
+## Contribution
+If you have a Telegram channel and you want to add it to this project or you know Telegram channels that can be used in this project you can ask for addition in Issues.
+
+## License
+This project is licensed under the MIT License - see the LICENSE file for details.
---
+++ index.php
@@ -0,0 +1,184 @@
+<?php
+
+header("Content-type: application/json;");
+
+include "modules/getv2ray.php";
+include "modules/config.php";
+
+function remove_duplicate_vmess($input)
+{
+    $array = explode("\n", $input);
+    foreach ($array as $item) {
+        $parts = decode_vmess($item);
+        $part_ps = $parts["ps"];
+        unset($parts["ps"]);
+        if (count($parts) >= 3) {
+            ksort($parts);
+            $part_serialize = serialize($parts);
+            $result[$part_serialize][] = $part_ps ?? "";
+        }
+    }
+    $finalResult = [];
+    foreach ($result as $serial => $ps) {
+        $partAfterHash = $ps[0] ?? "";
+        $part_serialize = unserialize($serial);
+        $part_serialize["ps"] = $partAfterHash;
+        $finalResult[] = encode_vmess($part_serialize);
+    }
+    $output = "";
+    foreach ($finalResult as $config) {
+        $output .= $output == "" ? $config : "\n" . $config;
+    }
+    return $output;
+}
+
+function remove_duplicate_xray($input, $type)
+{
+    $array = explode("\n", $input);
+
+    foreach ($array as $item) {
+        $parts = parseProxyUrl($item, $type);
+        $part_hash = $parts["hash"];
+        unset($parts["hash"]);
+        ksort($parts["params"]);
+        $part_serialize = serialize($parts);
+        $result[$part_serialize][] = $part_hash ?? "";
+    }
+
+    $finalResult = [];
+    foreach ($result as $url => $parts) {
+        $partAfterHash = $parts[0] ?? "";
+        $part_serialize = unserialize($url);
+        $part_serialize["hash"] = $partAfterHash;
+        $finalResult[] = buildProxyUrl($part_serialize, $type);
+    }
+
+    $output = "";
+    foreach ($finalResult as $config) {
+        $output .= $output == "" ? $config : "\n" . $config;
+    }
+    return $output;
+}
+
+function remove_duplicate_ss($input)
+{
+    $array = explode("\n", $input);
+
+    foreach ($array as $item) {
+        $parts = ParseShadowsocks($item);
+        $part_hash = $parts["name"];
+        unset($parts["name"]);
+        ksort($parts);
+        $part_serialize = serialize($parts);
+        $result[$part_serialize][] = $part_hash ?? "";
+    }
+
+    $finalResult = [];
+    foreach ($result as $url => $parts) {
+        $partAfterHash = $parts[0] ?? "";
+        $part_serialize = unserialize($url);
+        $part_serialize["name"] = $partAfterHash;
+        $finalResult[] = BuildShadowsocks($part_serialize);
+    }
+
+    $output = "";
+    foreach ($finalResult as $config) {
+        $output .= $output == "" ? $config : "\n" . $config;
+    }
+    return $output;
+}
+
+function get_reality($input)
+{
+    $array = explode("\n", $input);
+    $output = "";
+    foreach ($array as $item) {
+        if (stripos($item, "reality")) {
+            $output .= $output === "" ? $item : "\n$item";
+        }
+    }
+    return $output;
+}
+
+$mix_data = "";
+$vmess_data = "";
+$trojan_data = "";
+$vless_data = "";
+$shadowsocks_data = "";
+
+for ($p = count($Channel) - 1; $p >= 0; $p--) {
+    $CH = $Channel[$p];
+    for (
+        $type_count = count($Types[$CH]) - 1;
+        $type_count >= 0;
+        $type_count--
+    ) {
+        if ($Types[$CH][$type_count] === "vmess") {
+            if ($vmess_data === "") {
+                $vmess_data = get_v2ray($CH, $Types[$CH][$type_count], "text");
+            } else {
+                $vmess_data .=
+                    "\n" . get_v2ray($CH, $Types[$CH][$type_count], "text");
+            }
+        } elseif ($Types[$CH][$type_count] === "vless") {
+            if ($vless_data === "") {
+                $vless_data = get_v2ray($CH, $Types[$CH][$type_count], "text");
+            } else {
+                $vless_data .=
+                    "\n" . get_v2ray($CH, $Types[$CH][$type_count], "text");
+            }
+        } elseif ($Types[$CH][$type_count] === "trojan") {
+            if ($trojan_data === "") {
+                $trojan_data = get_v2ray($CH, $Types[$CH][$type_count], "text");
+            } else {
+                $trojan_data .=
+                    "\n" . get_v2ray($CH, $Types[$CH][$type_count], "text");
+            }
+        } elseif ($Types[$CH][$type_count] === "ss") {
+            if ($shadowsocks_data === "") {
+                $shadowsocks_data = get_v2ray(
+                    $CH,
+                    $Types[$CH][$type_count],
+                    "text"
+                );
+            } else {
+                $shadowsocks_data .=
+                    "\n" . get_v2ray($CH, $Types[$CH][$type_count], "text");
+            }
+        }
+    }
+}
+
+$fixed_string_vmess = remove_duplicate_vmess($vmess_data);
+$string_vless = str_replace("&amp;", "&", $vless_data);
+$fixed_string_vless = remove_duplicate_xray($string_vless, "vless");
+$string_trojan = str_replace("&amp;", "&", $trojan_data);
+$fixed_string_trojan = remove_duplicate_xray($string_trojan, "trojan");
+$fixed_string_shadowsocks = remove_duplicate_ss($shadowsocks_data);
+$fixed_string_reality = get_reality($fixed_string_vless);
+
+$mix_data =
+    $fixed_string_vmess .
+    "\n" .
+    $fixed_string_vless .
+    "\n" .
+    $fixed_string_trojan .
+    "\n" .
+    $fixed_string_shadowsocks;
+
+file_put_contents("sub/mix", $mix_data);
+file_put_contents("sub/vmess", $fixed_string_vmess);
+file_put_contents("sub/vless", $fixed_string_vless);
+file_put_contents("sub/trojan", $fixed_string_trojan);
+file_put_contents("sub/shadowsocks", $fixed_string_shadowsocks);
+file_put_contents("sub/mix_base64", base64_encode($mix_data));
+file_put_contents("sub/vmess_base64", base64_encode($fixed_string_vmess));
+file_put_contents("sub/vless_base64", base64_encode($fixed_string_vless));
+file_put_contents("sub/trojan_base64", base64_encode($fixed_string_trojan));
+file_put_contents(
+    "sub/shadowsocks_base64",
+    base64_encode($fixed_string_shadowsocks)
+);
+file_put_contents("sub/reality", $fixed_string_reality);
+file_put_contents("sub/reality_base64", base64_encode($fixed_string_reality));
+?>
